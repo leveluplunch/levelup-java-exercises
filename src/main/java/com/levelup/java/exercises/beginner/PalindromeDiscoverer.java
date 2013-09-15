2d5126cdddbda12845866de631e8538497a1ddea
Justin Musgrove <justin.musgrove@gmail.com>
2013-09-15 09:49:39 -0500

    added palindrome exercise

---
+++ src/main/java/com/levelup/java/exercises/beginner/PalindromeDiscoverer.java
@@ -0,0 +1,62 @@
+package com.levelup.java.exercises.beginner;
+
+import java.util.Scanner;
+
+public class PalindromeDiscoverer {
+
+//	Write program in java that accepts users input and validates if the input is a palindrome.  
+//	A http://en.wikipedia.org/wiki/Palindrome is a word that reads the same forwards as it does backwards.
+//	For this example we will consider a palindrome a palindrome without punctuation.
+	
+//	Levelup:
+//	validate input
+//	allow user to enter in more than one check
+//	a palindrome is often considered true excluding spaces or punctuation, enhance the program to punctuation 
+	
+	public static void main(String[] args) {
+		
+		// Create a Scanner object for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+
+		// Get user input 
+		System.out.print("Enter a word or phrase: ");
+		String userInput = keyboard.nextLine();
+		
+		boolean isAPalindrome = isPalindrome(userInput);
+		
+		if (isAPalindrome) {
+			System.out.print("The word or phrase is a palindrome");	
+		} else {
+			System.out.print("Sorry the word or phrase is NOT a palindrome");
+		}
+	}
+
+	
+	/**
+	 * Method should return true if a string is identified as a palindrome. 
+	 * There are many ways to do a palindrome check, this is just one of them.
+	 * If you are performing checks on very, very long strings you may want to 
+	 * consider another algorithm.
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isPalindrome(String str) {
+	
+		if (str.length() <= 1) {
+			return true;
+		} else {
+			String toCompare = str.replaceAll("\\s+","");
+			
+			StringBuffer buffer = new StringBuffer(toCompare);
+			String reversedString = buffer.reverse().toString();
+			
+			if (reversedString.equalsIgnoreCase(toCompare)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	
+}
---
+++ src/test/java/com/levelup/java/exercises/beginner/PalindromeDiscovererTest.java
@@ -0,0 +1,45 @@
+package com.levelup.java.exercises.beginner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for {@link PalindromeDiscoverer}
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/excercises'></a>
+ * 
+ */
+public class PalindromeDiscovererTest {
+
+	
+	@Test
+	public void is_a_palindrome_single_char () {
+		assertTrue(PalindromeDiscoverer.isPalindrome("A"));
+	}
+	
+	@Test
+	public void is_a_palindrome () {
+		assertTrue(PalindromeDiscoverer.isPalindrome("bob"));
+	}
+
+	@Test
+	public void is_a_palindrome_mixed_case () {
+		assertTrue(PalindromeDiscoverer.isPalindrome("A but tuba"));
+	}
+
+	@Test
+	public void is_a_palindrome_longer_phrase () {
+		assertTrue(PalindromeDiscoverer.isPalindrome("Able was I ere I saw Elba"));
+	}
+	
+	@Test
+	public void is_not_a_palindrome () {
+		assertFalse(PalindromeDiscoverer.isPalindrome("Free beer, I am there..."));
+	}
+	
+	
+}
