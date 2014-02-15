a232de94bdeec384acaa46102b5557bac27c0801
Justin Musgrove <justin.musgrove@gmail.com>
2014-02-15 13:02:38 -0600

    Added combination generator

---
+++ src/main/java/com/levelup/java/exercises/beginner/CombinationGenerator.java
@@ -0,0 +1,47 @@
+package com.levelup.java.exercises.beginner;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Sets;
+
+/**
+ * Write a program that generate all combinations of a set returning only that satisfy a size of n.
+ * 
+ * @author Justin Musgrove
+ * @see <a href='http://www.leveluplunch.com/java/exercises/combination-generator/'>Combination generator</a>
+ */
+public class CombinationGenerator {
+
+	public static void main(String[] args) {
+
+		Set<Integer> general = Sets.newHashSet(1, 2, 3, 4);
+
+		Set<Integer> combinations = generateCombinations(general, 3);
+		
+		System.out.println(combinations);
+	}
+
+	/**
+	 * Method will generate possible combinations of 
+	 * elements based on parameters
+	 * 
+	 * @param from
+	 * @param size
+	 * 
+	 * @return possible combinations
+	 * 
+	 */
+	public static Set<Integer> generateCombinations(
+			Set<Integer> from,
+			int size) {
+
+		Set<Set<Integer>> elements = Sets.powerSet(from);
+
+		Set<Integer> possibleCombinations = elements.stream().filter(p -> p.size() == size)
+				.collect(Collectors.toSet());
+
+		return possibleCombinations;
+	}
+
+}
